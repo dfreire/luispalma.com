e35020d545afbcdce5643f803d4e8aab3670b2dd
dfreire <dario.freire@gmail.com>
2016-03-02 21:32:59 +0000

    update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -25,11 +25,3 @@
             <?php snippet('menu') ?>
         </header>
     </div>
-
-
-  <!-- <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header> -->
