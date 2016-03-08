9d144c662a251babdec5b68f7914be4dff8e9d71
dfreire <dario.freire@gmail.com>
2016-03-08 18:56:13 +0000

    update

--- site/templates/gallery.php
+++ site/templates/gallery.php
@@ -5,15 +5,30 @@
     <?php if($page->isTitleVisible() == '1'): ?>
         <h2 class="title"><?php echo $page->title()->html() ?></h2>
     <?php endif ?>
-    <?php foreach ($page->images() as $image): ?>
-    	<a class="image-link" href="<?php echo $image->url() ?>"><?php echo thumb($image, array('width' => 200)) ?></a>
-    <?php endforeach ?>
+    <div class="gallery">
+        <?php foreach ($page->images() as $image): ?>
+            <a class="image-link" href="<?php echo $image->url() ?>"><?php echo thumb($image, array('width' => 220)) ?></a>
+        <?php endforeach ?>
+    </div>
 </div>
 
 <?php snippet('footer') ?>
 <script type="text/javascript">
     $(document).ready(function() {
-        $('.image-link').magnificPopup({ type: 'image' });
+        $(document).ready(function() {
+        	$('.gallery').magnificPopup({
+        		delegate: 'a',
+        		type: 'image',
+        		gallery: {
+        			enabled: true,
+        			preload: [0,1],
+                    tCounter: '%curr% / %total%'
+        		},
+        		image: {
+                    cursor: null
+                }
+        	});
+        });
     });
 </script>
 <?php snippet('outro') ?>
