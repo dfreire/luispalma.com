7c9edc55f7666b40f5fe8725e8acbb7ccda7177a
dfreire <dario.freire@gmail.com>
2016-03-08 19:35:25 +0000

    update

--- site/templates/gallery.php
+++ site/templates/gallery.php
@@ -6,29 +6,39 @@
         <h2 class="title"><?php echo $page->title()->html() ?></h2>
     <?php endif ?>
     <div class="gallery">
-        <?php foreach ($page->images() as $image): ?>
-            <a class="image-link" href="<?php echo $image->url() ?>"><?php echo thumb($image, array('width' => 220)) ?></a>
-        <?php endforeach ?>
+        <div class="grid">
+            <?php foreach ($page->images() as $image): ?>
+                <a class="image-link grid-item" href="<?php echo $image->url() ?>"><?php echo thumb($image, array('width' => 150)) ?></a>
+            <?php endforeach ?>
+        </div>
     </div>
 </div>
 
 <?php snippet('footer') ?>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/masonry/4.0.0/masonry.pkgd.min.js"></script>
+
 <script type="text/javascript">
     $(document).ready(function() {
-        $(document).ready(function() {
-        	$('.gallery').magnificPopup({
-        		delegate: 'a',
-        		type: 'image',
-        		gallery: {
-        			enabled: true,
-        			preload: [0,1],
-                    tCounter: '%curr% / %total%'
-        		},
-        		image: {
-                    cursor: null
-                }
-        	});
+
+        $('.grid').masonry({
+          // options
+          itemSelector: '.grid-item',
+          columnWidth: 175
         });
+
+    	$('.gallery').magnificPopup({
+    		delegate: 'a',
+    		type: 'image',
+    		gallery: {
+    			enabled: true,
+    			preload: [0, 1],
+                tCounter: '%curr% / %total%'
+    		},
+    		image: {
+                cursor: null
+            }
+    	});
     });
 </script>
 <?php snippet('outro') ?>
